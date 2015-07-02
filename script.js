75862ebc3350cf792146876a077c4b189b914ab0
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2015-07-02 19:29:53 +0900

    Make md editor

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-alert("test");
+var Vue = require("vue");
+var marked = require("marked");
+var editor = new Vue({
+  el: '#editor',
+  data: {
+    input: '# hello',
+    filename: null
+  },
+  filters: {
+    marked: marked
+  }
+});
